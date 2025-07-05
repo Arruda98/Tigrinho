fe423ad34580b9073866a11c90ccff7175c5ccea
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-05 17:02:01 -0300

    Add files via upload
    JOGO DO TIGRINHO!
    
    Para essa atividade, você faça um programa que imite o jogo do tigrinho.
    Para isso o programa iniciar cumprimentando o usuário, solicitar um depósito, e iniciar o jogo.
    O programa deve selecionar um número aleatório, e o usuário terá que adivinhá-lo. Para cada tentativa mal sucedida, deve ser descontado o valor de 1 do saldo do jogador.
    Para cada tentativa bem sucedida, o usuário ganhará 2.
    A cada tentativa bem sucedida do usuário, o programa deve mudar o número sorteado.
    
    A tentativa só pode ser realizada se o usuário tiver saldo.
    Caso o usuário erre, e seja descontado o valor da conta dele
    para uma nova tentativa, o usuário deve ter saldo suficiente,
    e caso o usuário não tenha saldo suficiente, o programa deve encerrar.
    O programa deve ter uma opção de encerrar as tentativas.
    
    Ao encerrar o programa, deve ser mostrado o saldo restante que o usuário possui na conta.

---
+++ Desafio4/Desafio4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Desafio4
+{
+    class JogoDoTigrinho
+    {
+        public static void Main (string[] args)
+        {
+            
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            double resposta = 0;
+            Console.WriteLine("Bem-Vindo a Koj1.bet");
+            Console.WriteLine("");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("O que você deseja fazer?\n");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("1 - Depositar");
+            Console.WriteLine("2 - Iniciar o jogo");
+            Console.WriteLine("3 - Encerrar o jogo\n");
+
+            resposta = double.Parse(Console.ReadLine());
+
+            switch (resposta)
+            {
+                case 1: Depositar(); break;
+                //case 2: Iniciar(); break;
+                //case 3: Encerrar(); break;
+                //case 4: 
+            }
+
+        }
+
+        public static void Depositar()
+        {
+            Console.Clear();
+
+            double deposito = 0;
+
+            Console.WriteLine("Vamos iniciar fazendo o depósito,");
+            Console.WriteLine("Escolha quanto deseja depositar:\n1 - R$ 5,00\n2 - R$ 10,00\n3 - R$ 15,00\n");
+            deposito = double.Parse(Console.ReadLine());
+
+            Menu();
+        }
+
+    }
+}
